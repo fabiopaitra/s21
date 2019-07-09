7e2a2883a65163fd2a4311b3b3da5d9947a8705c
Fabio Paitra <fpaitra@gmail.com>
2019-07-09 11:36:19 -0300

    Test formuário

--- src/components/home/10-faq.vue
+++ src/components/home/10-faq.vue
@@ -1,19 +1,15 @@
 /<template lang="pug">
 section.section.container
-  .columns
-    .column.content
-      h2.title Perguntas frequentes
-  .columns
-    .column.content
-      h4.subtitle Pergunta 01
-      p 
-      h4.subtitle Pergunta 01
-      p 
-    .column.content
-      h4.subtitle Pergunta 01
-      p 
-      h4.subtitle Pergunta 01
-      p 
+  form(name='contact', method='POST', data-netlify='true')
+    .field.has-addons
+      .control.has-icons-left.has-icons-right
+          input.input(type='text', placeholder='Coloque sua URL (ex: uber.com)')
+          span.icon.is-small.is-left
+            i.fas.fa-globe
+      .control
+        button.button.is-primary
+          | Analiste meu website
+    
     
 
       
